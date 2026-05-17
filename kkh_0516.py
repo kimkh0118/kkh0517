ada1a2db1f3b7bddd68e77023dcfa94e4517ded9
dongba0118 <kang7759137@gmail.com>
2026-05-17 14:15:29 +0900

    fix fetch url and db table name

--- kkh_0516.py
+++ kkh_0516.py
@@ -15,7 +15,6 @@ db = mysql.connector.connect(
 )
 
 cursor = db.cursor()
-cursor.execute("DROP TABLE IF EXISTS test_table;")
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS test_table (
     id INT AUTO_INCREMENT PRIMARY KEY,
